5c3de86f22ecea62ac0c22dcd931b0a5e0dff7d6
Mahesh Kafle <kafledarkhorse@gmail.com>
2022-05-15 01:51:20 +0400

    Added testcases for EveningDressPage

--- src/test/java/com/automationpractice/qa/testcases/EveningDressPageTest.java
+++ src/test/java/com/automationpractice/qa/testcases/EveningDressPageTest.java
@@ -3,6 +3,7 @@ package com.automationpractice.qa.testcases;
 import com.automationpractice.qa.base.TestBase;
 import com.automationpractice.qa.pages.EveningDressPage;
 import com.automationpractice.qa.pages.LoginPage;
+import com.automationpractice.qa.pages.PrintedDressPage;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -12,6 +13,7 @@ public class EveningDressPageTest extends TestBase {
 
     LoginPage loginPage;
     EveningDressPage eveningDressPage;
+    PrintedDressPage printedDressPage;
 
     public EveningDressPageTest(){
         // BaseClass constructor will be called and properties will be initialized
@@ -22,13 +24,14 @@ public class EveningDressPageTest extends TestBase {
     public void setUp(){
         initialization();
         loginPage =  new LoginPage();
-        eveningDressPage = loginPage.NavigateToEveningDressesPage();;
+        eveningDressPage = loginPage.NavigateToEveningDressesPage();
     }
 
     @Test(priority = 1)
-    public void verifyEveningDressPageTest(){
+    public void verifyNavigateToPrintedDressPage(){
         String eveningDressPageTitle = eveningDressPage.verifyEveningDressPageTitle();
         Assert.assertEquals(eveningDressPageTitle, "EVENING DRESSES ", "EveningDresses Page Title Not Matched");
+        printedDressPage =  eveningDressPage.NavigateToPrintedDressPage();
     }
 
     @AfterMethod
